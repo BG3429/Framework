4df03e608d9579eba8b0a33df9d656b232b780fa
BERGOT Gaëtan <bergot.gaetan01@gmail.com>
2018-04-25 10:06:26 +0200

    V2

--- tp2/app/controllers/Test.php
+++ tp2/app/controllers/Test.php
@@ -6,6 +6,25 @@ namespace controllers;
 class Test extends ControllerBase{
 
 	public function index(){
-		
+		echo "Hello World";
+	}
+	
+	public function hello($message="Salut",$destinataire="tout le monde"){
+		echo $message." ".$destinataire;
+	}
+	
+	public function afficher($message="I SEXY AND I KNOW IT",$type="",$icon="info"){
+		$this->loadView('Test/afficher.html',["msg"=>$message,"type"=>$type,"icon"=>$icon]);
+	}
+	
+	public function message(){
+		$message=[
+				["msg"=>"hello","icon"=>"user"],
+				["msg"=>"tu prends une douche","icon"=>"shower"],
+				["msg"=>"404 Fatal Error","icon"=>"frown"]
+		];
+		foreach ($message as $message){
+			$this->loadView('Test/afficher.html',$message);
+		}
 	}
 }
